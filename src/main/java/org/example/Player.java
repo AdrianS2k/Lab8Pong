06a184b1157bedcabace3ac6f34641cc7d27d65b
marco-menashe <mmenashe@calpoly.edu>
2025-05-21 18:19:48 -0700

    Update Player.java

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -1,4 +1,84 @@
 package org.example;
 
+import java.beans.PropertyChangeSupport;
+
 public class Player {
+
+    protected int score;
+    protected int barPos;
+    protected String chatMessageIn;
+    protected String chatMessageOut;
+
+    public Player() {
+        reset();
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void addScore(int points) {
+        this.score += points;
+    }
+
+    public int getBarPos() {
+        return barPos;
+    }
+
+    public void setBarPos(int barPos) {
+        this.barPos = barPos;
+    }
+
+    public String getChatMessageIn() {
+        return chatMessageIn;
+    }
+
+    public void setChatMessageIn(String chatMessageIn) {
+        this.chatMessageIn = chatMessageIn;
+    }
+
+    public String getChatMessageOut() {
+        return chatMessageOut;
+    }
+
+    public void setChatMessageOut(String chatMessageOut) {
+        this.chatMessageOut = chatMessageOut;
+    }
+
+    public void reset() {
+        this.score = 0;
+        this.barPos = 0;
+        this.chatMessageIn = "";
+        this.chatMessageOut = "";
+    }
+
+    public static class Host extends Player {
+        private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+
+        public Host() {
+            super();
+        }
+
+        @Override
+        public void setBarPos(int barPos) {
+            int oldBarPos = this.barPos;
+            this.barPos = barPos;
+            pcs.firePropertyChange("HostBarPosition", oldBarPos, barPos);
+        }
+    }
+
+    public static class Client extends Player {
+        private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+
+        public Client() {
+            super();
+        }
+
+        @Override
+        public void setBarPos(int barPos) {
+            int oldBarPos = this.barPos;
+            this.barPos = barPos;
+            pcs.firePropertyChange("ClientBarPosition", oldBarPos, barPos);
+        }
+    }
 }
