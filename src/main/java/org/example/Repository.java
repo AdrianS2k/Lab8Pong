21845e010cc42dae17ce20ff8d5c3f71ffac252c
marco-menashe <mmenashe@calpoly.edu>
2025-05-21 18:17:42 -0700

    Update Repository.java

--- src/main/java/org/example/Repository.java
+++ src/main/java/org/example/Repository.java
@@ -3,6 +3,8 @@ package org.example;
 // import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
+import javax.net.ssl.HostnameVerifier;
+
 
 public class Repository {
 
@@ -11,7 +13,9 @@ public class Repository {
     private final Player client = new Player();
     private int count = 0;
     private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
-
+    public int hostBarPos = host.getBarPos();
+    public int clientBarPos = client.getBarPos();
+    
     public Repository getInstance() {
         if (instance == null) {
             instance = new Repository();
@@ -19,65 +23,7 @@ public class Repository {
         return instance;
         }
 
+    // public Player getPlayer1() { return player1; }
+    // public Player getPlayer2() { return player2; }
     
-    public class Player {
-        private int playerNumber;
-        private int score;
-        private int barPosition;
-        private String chatMessageIn;
-        private String chatMessageOut;
-        
-        public Player() {
-            
-            this.score = 0;
-        }
-        
-        public int getPlayerNumber() {
-            return playerNumber;
-        }
-        
-        public int getScore() {
-            return score;
-        }
-        
-        public void addScore(int points) {
-            this.score += points;
-        }
-        public int getBarPosition() {
-            return barPosition;
-        }
-        public void setBarPosition(int barPosition) {
-            int oldbarPosition = this.barPosition;
-            this.barPosition = barPosition;
-            pcs.firePropertyChange("barPosition", oldbarPosition, barPosition);
-        }
-        public String getChatMessageIn() {
-            return chatMessageIn;
-        }
-        public void setChatMessageIn(String chatMessageIn) {
-            this.chatMessageIn = chatMessageIn;
-        }
-        public String getChatMessageOut() {
-            return chatMessageOut;
-        }
-        public void setChatMessageOut(String chatMessageOut) {
-            this.chatMessageOut = chatMessageOut;
-        }
-        public void reset() {
-            this.score = 0;
-            this.barPosition = 0;
-            this.chatMessageIn = "";
-            this.chatMessageOut = "";
-        }
-    }
-
-    
-
-
-    public Player getPlayer1() { return host; }
-    public Player getPlayer2() { return client; }
-    
-   
-    
-    }
-
+}
